e144f3a85bc2a8c9ae24311f5504edc55028ea99
Jonian Guveli <jonian@hardpixel.eu>
2017-12-11 14:50:56 +0200

    update engine

--- lib/bedrock_sass/engine.rb
+++ lib/bedrock_sass/engine.rb
@@ -1,7 +1,7 @@
 module BedrockSass
   class Engine < ::Rails::Engine
     initializer 'bedrock_sass.assets.precompile', group: :all do |app|
-      app.config.assets.paths << root.join('assets').to_s
+      app.config.assets.paths << root.join('assets', 'bedrock').to_s
       app.config.assets.paths << root.join('assets', '_vendor').to_s
     end
   end
